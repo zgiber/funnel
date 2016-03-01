bedf0f09d8a86cf0f179a2f9a6db666b5d4da2f3
Zoltan Giber <zoltan@typeform.com>
2016-03-01 18:13:40 +0100

    Example app

---
+++ cmd/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"log"
+	"math/rand"
+	"time"
+
+	"github.com/zgiber/funnel"
+)
+
+func main() {
+	g, err := funnel.NewDataDogBackend("127.0.0.1:8125", "testapp")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	m := &funnel.Metric{}
+	m.Name = "testmeasurement"
+	m.TimeStampFormat = funnel.Milliseconds
+	m.Type = funnel.GaugeType
+
+	for {
+		time.Sleep(500 * time.Millisecond)
+		g.Gather(random(m))
+	}
+}
+
+func random(m *funnel.Metric) funnel.DataPoint {
+	return funnel.NewDataPoint(m, rand.Float64(), time.Now().UTC(), map[string]interface{}{"test1": "somevalue", "test2": 23})
+}
